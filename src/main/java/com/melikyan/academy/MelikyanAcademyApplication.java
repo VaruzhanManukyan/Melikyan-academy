bcdcef06d344689f9928446367c4a0e812f735f1
VaruzhanManukyan <varuzhan04@gmail.com>
2026-03-27 01:55:26 +0400

    first commit

---
+++ src/main/java/com/melikyan/academy/MelikyanAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.melikyan.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelikyanAcademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MelikyanAcademyApplication.class, args);
+    }
+
+}
